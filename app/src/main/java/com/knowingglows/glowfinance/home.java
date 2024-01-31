11b0c2dc2e64d005e9ffbc172b97426126a8e7a7
KnowingGlows <knowingglows@gmail.com>
2024-01-31 20:36:34 +0545

    0.15 Added Google SignIn/Signup

--- app/src/main/java/com/knowingglows/glowfinance/home.java
+++ app/src/main/java/com/knowingglows/glowfinance/home.java
@@ -1,14 +1,40 @@
 package com.knowingglows.glowfinance;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
 
-public class home extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class home extends AppCompatActivity
+{
+    //creating variables
+    String income_des, expense_des, income_src, expense_src, income_name, expense_name;
+    Integer income_amount, expense_amount;
+    Date income_src_date, expense_src_date;
+
+    FirebaseUser firebaseUser;
+    FirebaseAuth firebaseAuth;
+
+    AppCompatTextView testtxtemail;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        testtxtemail = findViewById(R.id.testtxtemail);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        String email = firebaseUser.getEmail().toString();
+        testtxtemail.setText(email);
+
+
     }
 }
--- app/src/main/java/com/knowingglows/glowfinance/signup.java
+++ app/src/main/java/com/knowingglows/glowfinance/signup.java
@@ -1,6 +1,7 @@
 package com.knowingglows.glowfinance;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
@@ -10,37 +11,58 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Objects;
 
-public class signup extends AppCompatActivity
-{
+public class signup extends AppCompatActivity {
     FirebaseAuth auth;
+    public static final int RC_SIGN_IN = 100;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-
-        //firebase link
+        // Firebase link
         auth = FirebaseAuth.getInstance();
 
-        //calling activity
+        // Calling activity
         AppCompatButton signupbtn = findViewById(R.id.user_signupbtn);
         AppCompatEditText username = findViewById(R.id.user_username);
         AppCompatEditText useremail = findViewById(R.id.user_email);
         AppCompatEditText userpassword = findViewById(R.id.user_password);
         AppCompatButton loginbtn = findViewById(R.id.signin_btn);
+        AppCompatButton google_signupbtn = findViewById(R.id.google_signupbtn);
 
+        // Google Sign In configuration
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        google_signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent googleSignInClientSignInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(googleSignInClientSignInIntent, RC_SIGN_IN);
+            }
+        });
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,13 +71,13 @@ public class signup extends AppCompatActivity
             }
         });
 
-        //signup process
+        // Signup process
         signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user_name = username.getText().toString();
-                String user_password = userpassword.getText().toString();
-                String user_email = useremail.getText().toString();
+                String user_name = Objects.requireNonNull(username.getText()).toString();
+                String user_password = Objects.requireNonNull(userpassword.getText()).toString();
+                String user_email = Objects.requireNonNull(useremail.getText()).toString();
 
                 if (user_name.isEmpty()) {
                     username.setError("User Name Cannot Be Empty");
@@ -64,23 +86,65 @@ public class signup extends AppCompatActivity
                 } else if (user_password.isEmpty()) {
                     userpassword.setError("Password Cannot Be Empty");
                 } else {
-                    Task<AuthResult> authResultTask = auth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(signup.this, "Signup Successful!", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(signup.this, home.class));
-                            } else {
-                                Toast.makeText(signup.this, "Signup Failed. Please Try Again!" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+                    auth.createUserWithEmailAndPassword(user_email, user_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(signup.this, "Signup Successful!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(signup.this, home.class));
+                                    } else {
+                                        Toast.makeText(signup.this, "Signup Failed. Please Try Again!" +
+                                                Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
-
             }
         });
     }
+
     @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount signInAccount = accountTask.getResult(ApiException.class);
+                firebaseAuthWithGoogle(signInAccount);
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount googleSignInAccount) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        FirebaseUser firebaseUser = auth.getCurrentUser();
+                        if (authResult.getAdditionalUserInfo().isNewUser()) {
+                            Toast.makeText(signup.this, "Account Created!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(signup.this, home.class));
+                        } else {
+                            Toast.makeText(signup.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(signup.this, login.class));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(signup.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+     @Override
     public void onStart()
     {
         super.onStart();
@@ -92,4 +156,3 @@ public class signup extends AppCompatActivity
         }
     }
 }
-
