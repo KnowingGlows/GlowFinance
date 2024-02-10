98bcaf4eb3356b5e7e64d60a082918f977a362ca
KnowingGlows <knowingglows@gmail.com>
2024-02-10 09:53:49 +0545

    v 4.0 Minor Updates To Activities and UI Design Prep. for V1.0 LAUNCH UI

---
+++ app/src/main/java/com/knowingglows/glowfinance/glowcoinspage.java
@@ -0,0 +1,14 @@
+package com.knowingglows.glowfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class glowcoinspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glowcoinspage);
+    }
+}
--- app/src/main/java/com/knowingglows/glowfinance/home.java
+++ app/src/main/java/com/knowingglows/glowfinance/home.java
@@ -1,18 +1,24 @@
 package com.knowingglows.glowfinance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.AppCompatTextView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class home extends AppCompatActivity
@@ -27,6 +33,9 @@ public class home extends AppCompatActivity
     AppCompatTextView user_profilename, user_balance, user_expense;
 
     AppCompatImageView user_spendbehaviour;
+
+    HashMap<String, Object> user = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -36,6 +45,20 @@ public class home extends AppCompatActivity
         //firebase api connection
         firebaseAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+
+        //firestore linking
+        user.put("user_name", user_profilename);
+        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
 
         //instantiating activities
         profile_btn = findViewById(R.id.user_profile);
@@ -52,7 +75,7 @@ public class home extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+                startActivity(new Intent(home.this, profile.class));
             }
         });
 
@@ -61,7 +84,7 @@ public class home extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+                startActivity(new Intent(home.this, glowcoinspage.class));
             }
         });
 
@@ -70,7 +93,7 @@ public class home extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+             startActivity(new Intent(home.this, addrecords.class));
             }
         });
 
